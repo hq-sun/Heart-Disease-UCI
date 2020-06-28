235e6159bd071cafcdb25970582a97d0dc8dab5e
hq-sun <38084894+hq-sun@users.noreply.github.com>
2020-06-28 17:46:12 -0400

    Create 1-data cleaning.py

---
+++ scripts/1-data cleaning.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 27 20:31:03 2020
+
+@author: Heqing Sun
+"""
+
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Get current working directory
+os.getcwd()
+
+# Read csv file
+df = pd.read_csv("./data/raw/datasets-33180-43520-heart.csv")
+df_backup = df.copy()
+## 303 obs, 14 vars
+
+
+# =============================================================================
+# EDA
+# =============================================================================
+# Define some functions
+# For all variables
+def dataframe_description(df, col):
+    print('Column Name:', col)
+    print('Number of Rows:', len(df.index))
+    print('Number of Missing Values:', df[col].isnull().sum())
+    print('Percent Missing:', df[col].isnull().sum()/len(df.index)*100, '%')
+    print('Number of Unique Values:', len(df[col].unique()))
+    print('\n')
+
+# For continuous variables    
+def descriptive_stats_continuous(df, col):
+    print('Column Name:', col)
+    print('Mean:', np.mean(df[col]))
+    print('Median:', np.nanmedian(df[col]))
+    print('Standard Deviation:', np.std(df[col]))
+    print('Minimum:', np.min(df[col]))
+    print('Maximum:', np.max(df[col]))
+    print('\n')
+
+# Plotting distribution plots for continuous variables
+def plot_distribution(df, col):
+    sns.set(style='darkgrid')
+    ax = sns.distplot(df[col].dropna())
+    plt.xticks(rotation=90)
+    plt.title('Distribution Plot for ' + col)
+    plt.show()
+    
+# Plotting count plots for categorical variables 
+def plot_counts(df, col):
+    sns.set(style='darkgrid')
+    ax = sns.countplot(x=col, data=df)
+    plt.xticks(rotation=90)
+    plt.title('Count Plot')
+    plt.show()
+
+df.columns.values
+## 'age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'target'
+continuous_vars = ['trestbps', 'chol', 'thalach', 'oldpeak']
+categorical_vars = ['age', 'sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal', 'target']
+
+
+for col in list(df.columns.values):
+    dataframe_description(df, col)
+## No missing value in all columns
+
+for col in list(continuous_vars):
+    descriptive_stats_continuous(df, col)
+
+for col in list(continuous_vars):
+    plot_distribution(df, col)
+
+for col in list(categorical_vars):
+    plot_counts(df, col)
+
+# Check if target variable is imbalanced
+df.target.value_counts()
+# 1    165
+# 0    138
+## It is a balanced dataset
