cf08c552213a54c620998dc397ca36e793988298
hq-sun <hq-sun@users.noreply.github.com>
2020-07-03 23:11:54 -0400

    Add Logistic Regression Model

--- scripts/2-modeling.py
+++ scripts/2-modeling.py
@@ -91,3 +91,56 @@ shap.summary_plot(shap_values[1], X_test, plot_type="bar")
 ## The number of major vessels is the most important one based on shap
 
 shap.summary_plot(shap_values[1], X_test)
+
+# =============================================================================
+# Logistic Regression
+# =============================================================================
+from sklearn.linear_model import LogisticRegression
+model_lr = LogisticRegression()
+model_lr.fit(X_train, y_train)
+
+# For test data
+y_predict = model_rf.predict(X_test)
+y_pred_proba = model_rf.predict_proba(X_test)[:, 1]
+
+# Confusion Matrix
+confusion_matrix = confusion_matrix(y_test, y_predict)
+confusion_matrix
+
+# Different metrics
+# accuracy: (tp + tn) / (p + n)
+accuracy = accuracy_score(y_test, y_predict)
+print('Accuracy: %f' % accuracy)
+## Accuracy: 0.803279
+
+# precision tp / (tp + fp)
+precision = precision_score(y_test, y_predict)
+print('Precision: %f' % precision)
+## Precision: 0.787879
+
+# recall: tp / (tp + fn)
+recall = recall_score(y_test, y_predict)
+print('Recall: %f' % recall)
+## Recall: 0.838710
+
+# f1: 2 tp / (2 tp + fp + fn)
+f1 = f1_score(y_test, y_predict)
+print('F1 score: %f' % f1)
+## F1 score: 0.812500
+
+# Plot ROC Curve (Receiver Operator Curve)
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
+
+fig, ax = plt.subplots()
+ax.plot(fpr, tpr)
+ax.plot([0, 1], [0, 1], transform=ax.transAxes, ls="--", c=".3")
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.rcParams['font.size'] = 12
+plt.title('ROC Curve for Logistic Regression Model')
+plt.xlabel('False Positive Rate (1 - Specificity)')
+plt.ylabel('True Positive Rate (Sensitivity)')
+plt.grid(True)
+
+auc(fpr, tpr)
+## AUC: 0.8903225806451612
